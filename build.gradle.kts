7a514d432d8088907e6a2081ee01f8ba452e42d7
NUmeroAndDev <numero.dev.and@gmail.com>
2025-07-02 11:34:54 +0900

    Migrated from kotlinOptions to compilerOptions

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,15 +11,3 @@ plugins {
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kspPlugin) apply false
 }
-
-subprojects {
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>()
-        .configureEach {
-            kotlinOptions {
-                jvmTarget = JavaVersion.VERSION_11.toString()
-                compilerOptions
-                    .freeCompilerArgs
-                    .add("-Xcontext-receivers")
-            }
-        }
-}
